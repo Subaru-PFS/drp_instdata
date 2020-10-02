62ffd313b6a35a61839c1554ce474c5b730dc6f0
Paul Price <price@astro.princeton.edu>
2020-10-02 15:06:55 -0400

    add gain for detectors
    We were previously implicitly using a gain of unity, which is
    not the value the pipeline uses. The value of 1.24 added here
    is the average of the values for the different amplifiers (all
    cameras have the same values, currently).

--- data/detectors/NIR.py
+++ data/detectors/NIR.py
@@ -1,5 +1,6 @@
 pixelScale = 0.015
 ccdSize = (4176, 4096)  # Should be (4096,4096), but we're using red arm biases
 bias = 1000
+gain = 1.24
 readNoise = 4.1
 interCcdGap = 0.0
--- data/detectors/Red.py
+++ data/detectors/Red.py
@@ -2,6 +2,7 @@ pixelScale = 0.015
 ccdSize = (4176,4096)
 ccdCenter = ((ccdSize[0] + 48)/2, ccdSize[1]/2)
 bias = 1000
+gain = 1.24
 readNoise = 3.8
 interCcdGap = 1.040 # 31.440 (physical) - 30.720 (active) + 0.300 (physical gap) + 0.020 (observed)
 ccdXOffset = 0 # -20 * pixelScale
