da899f7a1d5cab83c507efd22f6d06d32de230af
Paul Price <price@astro.princeton.edu>
2023-06-15 12:09:23 -0400

    nir: set gain to same as for real data
    The camera.yaml in obs_pfs records the system gain for n1 as
    9, which differs from the value we were using here.
    Correct this to match the real data (and the pipeline).

--- data/detectors/NIR.py
+++ data/detectors/NIR.py
@@ -1,6 +1,12 @@
 pixelScale = 0.015
 ccdSize = (4096, 4096)
 bias = 100
-gain = 4.0
-readNoise = 10.0
+# This is the total system gain, same as in camera.yaml.
+# In the real data, the pixel values have had an initial gain from the ASIC
+# applied, which is recorded in the W_H4GAIN header keyword (typically around
+# 2.8), and this gain is removed from the system gain on read.
+# In the simulator, we set W_H4GAIN=1. This avoids having to change the pixel
+# values, but should have the correct effect.
+gain = 9.0
+readNoise = 20.0
 interCcdGap = 0.0
